d1fff678580c278278b0c8bf4fb6572971076f05
Alex Hort-Francis <60655956+hortfrancis@users.noreply.github.com>
2025-10-08 00:17:37 +0100

    chore: Fix code formatting

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,8 @@ import Hello from '@/content/hello.mdx';
 
 export default function Home() {
   return (
-    <div
-      className='content-from-markdown space-y-4'
-    >
+    <div className='content-from-markdown space-y-4'>
       <Hello />
     </div>
-
   );
 }
